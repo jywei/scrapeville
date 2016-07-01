bc7e77e67eeb5c6c699800583a306d46a8c265f5
Roy Wei <roywjy@gmail.com>
2016-07-01 23:12:54 +0800

    comment functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
+
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :newsvilles
+  resources :newsvilles do
+    resources :comments
+  end
   get 'pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
